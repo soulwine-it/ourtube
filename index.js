4227483c0a822a21224b1d493f9f0dc5a9917e3a
soulwine-it <skyorwind@gmail.com>
2020-02-19 18:39:09 +0900

    server test

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ const express = require("express");
 const app = express();
 
 const PORT = 4000;
+
 function handleListening() {
-  console.log(`Listening on: http://localhost:${PORT}`);
+    console.log(`Listening on: http://localhost:${PORT}`);
 }
+
+function handleHome(req, res) {
+    console.log(req);
+    res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+    res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 //4000 포트
 app.listen(PORT, handleListening);
