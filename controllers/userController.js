c54722a604695165c86392c5f070656822b4b45f
soulwine-it <skyorwind@gmail.com>
2020-02-20 20:58:07 +0900

    Log In HTML

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,22 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
+export const join = (req, res) => res.render("Join", {
+    pageTitle: "Videos"
+});
+export const login = (req, res) => res.render("Login", {
+    pageTitle: "Login"
+});
+export const logout = (req, res) => res.render("Logout", {
+    pageTitle: "Logout"
+});
 
-export const users = (req, res) => res.send("Users");
-export const userDeatil = (req, res) => res.send("user Detail");
-export const editProfile = (req, res) => res.send("edit Profile");
-export const changePassword = (req, res) => res.send("change Password");
+export const users = (req, res) => res.render("Users", {
+    pageTitle: "Users"
+});
+export const userDeatil = (req, res) => res.render("user Detail", {
+    pageTitle: "User Detail"
+});
+export const editProfile = (req, res) => res.render("edit Profile", {
+    pageTitle: "Edit Profile"
+});
+export const changePassword = (req, res) => res.render("change Password", {
+    pageTitle: "Change Password"
+});
