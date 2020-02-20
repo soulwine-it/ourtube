15762492e6d1221b78b7c11de8be2e366e4012b2
soulwine-it <skyorwind@gmail.com>
2020-02-20 18:43:37 +0900

    Template Variables in Pug

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import {
+  localsMiddleware
+} from "./middleWares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
+
 
 // // 디폴트 값이 아닐때는 아래와 같이 표현
 // import {
@@ -15,6 +19,8 @@ import routes from "./routes";
 
 const app = express();
 
+// middleware 보안 관련
+app.use(helmet());
 //view engine 설정값을 pug로 변경
 app.set('view engine', 'pug');
 //middleware 사용자의 정보를 담도록 해줌
@@ -27,10 +33,15 @@ app.use(
     extended: true
   })
 );
-// middleware 보안 관련
-app.use(helmet());
+
 //middleware logging
 app.use(morgan("dev"));
+
+// 미들웨어 만드는 방법
+// app.use((req, res, next) => {})
+// app.use(function (req, res, next) {})
+// const localMiddleware = (req, res, next) => {}
+app.use(localsMiddleware);
 
 //router
 app.use(routes.home, globalRouter);
@@ -38,57 +49,3 @@ app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
 export default app;
-
-// import express from "express";
-// import morgan from "morgan";
-// import helmet from "helmet";
-// import cookieParser from "cookie-parser";
-// import bodyParser from "body-parser";
-
-// //express 라는 파일을 찾고 못찾으면 node_module에서 검색
-// // const express = require("express");
-// const app = express();
-// //포트 번호
-// const PORT = 4000;
-
-// const handleListening = () =>
-//   console.log(`Listening on: http://localhost:${PORT}`);
-
-// const handleHome = (req, res) => res.send("Hello from my ass");
-
-// // middlewware 요청과 응답 사이의 시간
-// const betweenHome = (req, res, next) => {
-//   console.log("Between");
-//   next();
-// };
-
-// // function handleProfile(req, res) {
-//   // res.send("You are on my profile");
-// // }
-// //es 6 version
-// const handleProfile = (req, res) => {
-//   res.send("You are on my profile");
-// };
-
-// //middleware가 어디에 있는지가 중요
-// // app.use(betweenHome);
-// // middleware에 res.send("")를 추가하면 페이지가 멈춤
-
-// //cookie parser
-// app.use(cookieParser());
-
-// //body parser
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
-// app.use(helmet());
-// //morgan은 logging임
-// app.use(morgan("dev"));
-
-// app.get("/", handleHome);
-
-// app.get("/profile", handleProfile);
-
-// //4000 포트
-// // app.listen(PORT, handleListening);
