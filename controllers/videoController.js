39fcdfbd12e0b79c69cda631a28f793ec6017e9c
soulwine-it <skyorwind@gmail.com>
2020-02-20 20:14:53 +0900

    search controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,22 +1,39 @@
 export const home = (req, res) => res.render("home", {
+
     pageTitle: "Home"
 });
-export const search = (req, res) => res.render("Search", {
-    pageTitle: "Search"
-});
+export const search = (req, res) => {
+    //es6 (req.query.term)
+    const {
+        query: {
+            term: searchingBy
+        }
+    } = req;
+
+    console.log(req.query.term);
+    res.render("search", {
+        pageTitle: "Search",
+        searchingBy: searchingBy
+    });
+};
 
 export const videos = (req, res) => res.render("Videos", {
+
     pageTitle: "videos"
 });
 export const upload = (req, res) => res.render("Upload", {
+
     pageTitle: "upload"
 });
 export const videoDetail = (req, res) => res.render("Video Detail", {
+
     pageTitle: "videoDetail"
 });
 export const editVideo = (req, res) => res.render("Edit Video", {
+
     pageTitle: "editVideo"
 });
 export const deleteVideo = (req, res) => res.render("Delete Video", {
+
     pageTitle: "deleteVideo"
 });
