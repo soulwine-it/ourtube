6f7ea2c32a083831855a64cf46d18298ec788f24
soulwine-it <skyorwind@gmail.com>
2020-02-20 16:18:53 +0900

    install pug

--- app.js
+++ app.js
@@ -15,16 +15,24 @@ import routes from "./routes";
 
 const app = express();
 
+//view engine 설정값을 pug로 변경
+app.set('view engine', 'pug');
+//middleware 사용자의 정보를 담도록 해줌
 app.use(cookieParser());
+//middleware bodyParser 는 사용자가 웹사이트로 전달하는 정보들을 검사하는 미들웨어, request 정보에서
+//form이나 json 형태로 된 body를 검사
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
+// middleware 보안 관련
 app.use(helmet());
+//middleware logging
 app.use(morgan("dev"));
 
+//router
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
